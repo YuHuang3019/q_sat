b199a0d7652669571f32a7329ab5d241b0134419
YuHuang3019 <yh3019@columbia.edu>
2017-12-03 20:35:55 -0500

    travis

--- q_sat/tests/test_q_sat.py
+++ q_sat/tests/test_q_sat.py
@@ -5,7 +5,7 @@ from q_sat.q_sat import q_sat
 def test_q_sat():
     # some known results
     # T = 273.15[K], P=100000[Pa], es = 610.55[Pa], q=0.0037
-    # not strict test with rtol, approximation of above test values
+    # not strict test with rtol, use approximation of above test values
     q = q_sat(273.15, 1e5)
     print(q)
     np.testing.assert_allclose(q,0.0037,atol=1e-4)
